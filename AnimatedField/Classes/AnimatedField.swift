499390dd2599286f091950234ae18e4c3c6d6534
Nik Burnt <nikburnt@gmail.com>
2020-06-14 22:59:58 +0300

    fixed issue with counter

--- AnimatedField/Classes/AnimatedField.swift
+++ AnimatedField/Classes/AnimatedField.swift
@@ -429,7 +429,7 @@ extension AnimatedField {
     func updateCounterLabel() {
         let count = textView.text == attributedPlaceholder?.string && textView.textColor == UIColor.lightGray.withAlphaComponent(0.8) ? (textView.text.count - (attributedPlaceholder?.string.count ?? 0)) : textView.text.count
         let value = (dataSource?.animatedFieldLimit(self) ?? 0) - count
-        counterLabel.text = format.countDown ? "\(value)" : "\((textField.text?.count ?? 0) + 1)/\(dataSource?.animatedFieldLimit(self) ?? 0)"
+        counterLabel.text = format.countDown ? "\(value)" : "\((textField.text?.count ?? 0))/\(dataSource?.animatedFieldLimit(self) ?? 0)"
         if format.counterAnimation {
             counterLabel.transform = CGAffineTransform(scaleX: 1.05, y: 1.05)
             UIView.animate(withDuration: 0.3) { [weak self] in
